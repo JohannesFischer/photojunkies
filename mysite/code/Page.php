885cb3d645cfcbb1895f60aa50c91cae66f961ed
Johannes <git@johannes-fischer.de>
2013-06-07 13:21:42 +0800

    added lesscss module, moved layout styles to app.less

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -12,6 +12,21 @@ class Page_Controller extends ContentController {
 
 	public function init() {
 		parent::init();
+		
+		$theme_folder = 'themes/' . SSViewer::current_theme();
+		$css_folder = $theme_folder . '/css/';
+		
+		$css_files = array(
+			'foundation/normalize.css',
+			'foundation/foundation.min.css',
+			'typography.css',
+			'form.css',
+			'app.less'
+		);
+		
+		foreach ($css_files as $file) {
+			Requirements::css($css_folder . $file);
+		}
 	}
 	
 	public function getEntryImage() {
